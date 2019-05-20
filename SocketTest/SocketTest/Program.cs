714d5f47999c149ab52976998b10ae7605aa6ccd
mohamed-azikiou <50836748+mohamed-azikiou@users.noreply.github.com>
2019-05-20 16:41:41 +0200

    Add files via upload

---
+++ SocketTest/SocketTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketTest
+{
+    class Program
+    {
+        static int port = 4558;
+        private static readonly Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static Socket client;
+        private  readonly static byte[] buf = new byte[sizeof(double)];
+        static void Main(string[] args)
+        {
+            // Bind Server with the EndPoint
+            // IPAddress.Any paramiter to Listen on all network interfaces Port is the port
+            server.Bind(new IPEndPoint(IPAddress.Any, port));
+
+            // Listen to only one Device
+            server.Listen(1);
+
+            // Begin Accept will wait untill a connection comes
+            // First parameter is the Action to do when a Connection requested
+            // Second parameter is the State it may be used to send some Arguments so they can be retreived when connection requested
+            server.BeginAccept((IAsyncResult result) =>
+            {
+                // Accept request and get Client Socket
+                client = server.EndAccept(result);
+                // Execute ReceiveData when receiving sizeof(double) Data into buf starting from Zero Index
+                client.BeginReceive(buf, 0, sizeof(double), SocketFlags.None, F, null);
+            }, null);
+            Console.WriteLine("Hello World!");
+        }
+
+        static void F(IAsyncResult newr) {
+            client.EndAccept(newr);
+            double yourResult = BitConverter.ToDouble(buf, 0);
+            // yourResult is the received double
+
+
+            client.BeginReceive(buf, 0, sizeof(double), SocketFlags.None, F, null);
+        }
+    }
+}
